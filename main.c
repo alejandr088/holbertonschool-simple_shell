1551a3d512add71fbb7e30fc57e6a67aead8acb9
alejandr088 <alejandrivello@gmail.com>
2023-04-30 15:42:22 -0700

    implemented cd, env & exit funct

--- main.c
+++ main.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	char *line = NULL, **args = NULL;
-	char *prompt = "($) ";
+	char *line = NULL, **args = NULL, *prompt = "($) ";
+	int status = 0;
 	size_t len = 0;
 	ssize_t read_byte = 0;
-	int status = 0;
-
+	
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -24,25 +23,50 @@ int main(void)
 			break;
 
 		args = tokenize(line);
-		if (args != NULL && args[0] != NULL && strcmp(args[0], "env") == 0)
+		if (args[0] != NULL)
 		{
-			char **env_var = environ;
-			while (*env_var != NULL)
+			if (strcmp(args[0], "env") == 0)
 			{
-				printf("%s\n", *env_var);
-				env_var++;
+				char **env_var = environ;
+				while (*env_var != NULL)
+				{
+					printf("%s\n", *env_var);
+					env_var++;
+				}
+				status = 0;
 			}
-			status = 0;
+			else if (strcmp(args[0], "cd") == 0)
+			{
+				if (args[1] == NULL)
+				{
+					fprintf(stderr, "Error: expected argument to \"cd\"\n");
+					status = 1;
+				}
+				else
+				{
+					if (chdir(args[1]) != 0)
+					{
+						perror("Error");
+						status = 1;
+					}
+					else
+					{
+						status = 0;
+					}
+				}
+			}
+			else if (strcmp(args[0], "exit") == 0)
+			{
+				status = 0;
+				break;
+			}
+			else
+			{
+				status = execute_command(args);
+			}
 		}
-		else
-		{
-			status = execute_command(args);
-		}
-		status = execute_command(args);
-
 		free(args);
 	}
-
 	free(line);
 	return (status);
 }
