4d146cdef8c224b18b3345d61a86020895ebaed6
alejandr088 <alejandrivello@gmail.com>
2023-04-29 20:36:24 -0700

    fixed betty

--- hell.c
+++ hell.c
@@ -4,7 +4,7 @@
  * execute_command - Executes a command with its arguments
  * @args: An array of arguments to execute
  *
- * Return: 0 on success, 1 on error
+ * Return: The exit status of the command
  */
 int execute_command(char **args)
 {
@@ -13,45 +13,25 @@ int execute_command(char **args)
 
 	if (args == NULL || args[0] == NULL)
 		return (0);
-
-	if (strcmp(args[0], "exit") == 0)
-	{
-		exit(0);
-	}
-
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		perror("Error: fork");
 		return (1);
 	}
-
 	if (child_pid == 0)
 	{
-		char *exe_path = find_executable_path(args[0]);
-		if (exe_path == NULL)
-		{
-			fprintf(stderr, "/hsh: 1: %s: not found\n", args[0]);
-			exit(1);
-		}
-
-		if (execve(exe_path, args, NULL) == -1)
+		if (execve(args[0], args, NULL) == -1)
 		{
 			perror("Error: execve");
-			exit(1);
+			_exit(1);
 		}
 	}
 	else
 	{
 		wait(&status);
-
-		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
-
-		if (status != 0)
-			return (1);
 	}
-	return (0);
+	return (status);
 }
 
 /**
@@ -62,17 +42,15 @@ int execute_command(char **args)
  */
 char **tokenize(char *line)
 {
-	int bufsize = MAX_ARG_COUNT;
-	int position = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
+	int bufsize = MAX_ARG_COUNT, position = 0;
 	char *token;
+	char **tokens = malloc(bufsize * sizeof(char *));
 
 	if (!tokens)
 	{
 		fprintf(stderr, "Error: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-
 	token = strtok(line, TOK_DELIM);
 	while (token != NULL)
 	{
@@ -89,7 +67,6 @@ char **tokenize(char *line)
 				exit(EXIT_FAILURE);
 			}
 		}
-
 		token = strtok(NULL, TOK_DELIM);
 	}
 	tokens[position] = NULL;
--- main.h
+++ main.h
@@ -15,15 +15,10 @@
 #define TOK_DELIM " \t\r\n\a"
 
 /* Function Declarations */
-void loop(void);
-char *read_line(void);
-char **split_line(char *line);
 int execute_command(char **args);
 char **tokenize(char *input);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-char *find_executable_path(char *exe_name);
 
-/* Global variables */
 extern char **environ;
 
 #endif
